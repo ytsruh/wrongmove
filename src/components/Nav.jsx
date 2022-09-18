4ec55721105a68a0a2741700cfd4bfeff35fac18
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2022-09-18 16:47:12 +0100

    page layout

---
+++ src/components/Nav.jsx
@@ -0,0 +1,26 @@
+import Image from "next/image"
+import Link from "next/link"
+import wrongMoveLogoNav from '../assets/wrongMoveLogoNav.png'
+
+function Nav() {
+  return (
+    <nav>
+        <div className="nav-container container">
+            <div className="nav-left">
+                <Link href='/'><Image src={wrongMoveLogoNav} alt='wrongmove logo - click for home' /></Link>
+            </div>
+            <div className="nav-right">
+                <ul>
+                    <Link href='/buy'>Buy</Link>
+                    <Link href='/rent'>Rent</Link>
+                    <Link href='/buy'>House Prices</Link>
+                    <Link href='/find-agent'>Find Agent</Link>
+                    <Link href='/sign-in'>Sign In</Link>
+                </ul>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Nav
