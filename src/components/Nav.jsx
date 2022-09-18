fe73d5cdc1c4856b5c84d12df53933d089913532
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2022-09-18 22:46:06 +0100

    navbar (not responsive)

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,14 @@
 import Image from "next/image"
 import Link from "next/link"
-import wrongMoveLogoNav from '../assets/wrongMoveLogoNav.png'
+import wrongmoveLogoNav from '../assets/wrongmoveLogoNav.png'
+import iconUserPrimary24 from '../assets/icons/iconUserPrimary24.png'
 
 function Nav() {
   return (
     <nav>
         <div className="nav-container container">
             <div className="nav-left">
-                <Link href='/'><Image src={wrongMoveLogoNav} alt='wrongmove logo - click for home' /></Link>
+                <Link href='/'><Image src={wrongmoveLogoNav} alt='wrongmove logo - click for home' /></Link>
             </div>
             <div className="nav-right">
                 <ul>
@@ -15,7 +16,12 @@ function Nav() {
                     <Link href='/rent'>Rent</Link>
                     <Link href='/house-prices'>House Prices</Link>
                     <Link href='/find-agent'>Find Agent</Link>
-                    <Link href='/sign-in'>Sign In</Link>
+                    <Link className="btn btn-border" href='/sign-in'>
+                        <div className="sign-in-nav">
+                            <Image src={iconUserPrimary24} alt='user icon'/>
+                            <p>Sign In</p>
+                        </div>
+                    </Link>
                 </ul>
             </div>
         </div>
