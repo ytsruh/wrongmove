4fc82902d218bf2333af91e1d7defebfb4ccd15f
Chris Hurst <46472228+ytsruh@users.noreply.github.com>
2022-09-18 16:27:30 +0100

    Express app setup

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async rewrites() {
+    return [{ source: "/api/(.*)", destination: "/api" }];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
