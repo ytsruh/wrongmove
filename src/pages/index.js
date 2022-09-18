94d7e38b31065af35c2e9b3606abad3accb37dd3
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2022-09-18 14:37:37 +0100

    coming soon - logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,7 @@
 import Head from "next/head";
+import Image from 'next/image'
+
+import wrongmoveSmallLogo from '../assets/wrongmoveSmallLogo.png'
 
 export default function Home() {
   return (
@@ -9,7 +12,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div style={styles.div}>
-        <h1 style={styles.h1}>Wrongmove</h1>
+        <div className="comingSoonLogoDiv">
+          <h1>wrongmove</h1>
+          <Image src={wrongmoveSmallLogo} alt='wrongmove small logo'/>
+        </div>
         <h2 style={styles.h2}>Coming Soon</h2>
       </div>
     </div>
