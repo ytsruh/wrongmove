c9f5ae216920f0e0b804973cb5088f3c16a3412d
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2022-10-11 11:06:40 +0100

    home - recent searches

--- src/components/RecentSearches.jsx
+++ src/components/RecentSearches.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import InlineLink from "./InlineLink"
+import RecentSearch from "./cards/RecentSearch"
 
 function RecentSearches() {
   return (
     <div className='card card-border recent-searches'>
         <h3 className='effraBold'>Recent Searches</h3>
+        <RecentSearch />
+        <RecentSearch />
     </div>
   )
 }
