226af14a069723f464c0309ad42e2f4068f3f199
Chris Hurst <46472228+ytsruh@users.noreply.github.com>
2022-11-02 19:15:20 +0000

    Updated public endpoint & Prisma schema

--- app/routes/public.js
+++ app/routes/public.js
@@ -31,4 +31,17 @@ router.get("/sales/:id", async (req, res) => {
   }
 });
 
+// Get all agents
+router.get("/agents", async (req, res) => {
+  try {
+    const data = await prisma.agent.findMany();
+    // Send response back
+    res.status(200).json({ data });
+  } catch (error) {
+    // For errors, log to console and send a 500 response back
+    console.log(error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
 export default router;
