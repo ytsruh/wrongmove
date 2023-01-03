bd34a655d63e6a3fa281ef2a741825f4d8545f01
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2023-01-03 16:21:08 +0000

    edit listing

---
+++ src/pages/agent/edit-listing/[id].jsx
@@ -0,0 +1,140 @@
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+import Alert from '../../../components/Alert';
+import Form from '../../../components/Form'
+import Protected from '../../../components/Protected';
+import InlineLink from '../../../components/InlineLink'
+
+import useFetchData from '../../../hooks/useFetchData'
+
+function EditListing() {
+
+  const router = useRouter()
+  const { id } = router.query
+
+  const [loading, setLoading] = useState(true)
+  const [singleListing, setSingleListing] = useState(null)
+  const [errMsg, setErrMsg] = useState(false)
+
+  const { isLoading, serverError, apiData } = useFetchData(`/api/sales/${id}`)
+
+  useEffect(() => {
+    if (!router.isReady) return;
+    if (!apiData) return;
+    if (apiData.data.length === 0) {
+        return
+    } else setSingleListing(apiData)
+    setLoading(false)
+  }, [id, apiData, router.isReady])
+  
+  if (isLoading || loading) return <h1>Loading...</h1>;
+  if (serverError) return <h1>Server Error</h1>
+  
+  if (!isLoading && singleListing) {
+
+    const { address, price, propertyType, bedrooms, bathrooms, keyFeatures, description } = singleListing.data[0]
+
+    const editFormArr = [
+        {
+          label: 'Property Address',
+          name: 'address',
+          type: 'text',
+          placeholder: address
+        },
+        {
+          label: 'Price',
+          name: 'price',
+          type: 'number',
+          placeholder: price,
+        },
+        {
+          // change this to show radio boxes
+          label: 'Property Type',
+          name: 'propertyType',
+          type: 'text',
+          placeholder: propertyType
+        },
+        {
+          // should take up 50% width on desktop
+          label: 'Bedrooms',
+          name: 'bedrooms',
+          type: 'number',
+          placeholder: bedrooms
+        },
+        {
+          // should take up 50% width on desktop
+          label: 'Bathrooms',
+          name: 'bathrooms',
+          type: 'number',
+          placeholder: bathrooms
+        },
+        {
+          label: 'Key Features',
+          name: 'keyFeatures',
+          type: 'text',
+          placeholder: keyFeatures
+        },
+        {
+            label: 'Description',
+            name: 'description',
+            type: 'text',
+            placeholder: description
+        }
+    ]
+
+    const onSubmit = async (form, callback) => {
+        callback()
+
+        if(form.address === "") delete form.address
+        if(form.bathrooms === "") delete form.bathrooms
+        if(form.bedrooms === "") delete form.bedrooms
+        if(form.description === "") delete form.description
+        if(form.keyFeatures === "") delete form.keyFeatures
+        if(form.price === "") delete form.price
+        if(form.propertyType === "") delete form.propertyType
+        if(form.price) form.price = parseInt(form.price)
+        if(form.bathrooms) form.bathrooms = parseInt(form.bathrooms)
+        if(form.bedrooms) form.bedrooms = parseInt(form.bedrooms)
+
+        try {
+          const user = JSON.parse(sessionStorage.getItem('user'))
+          await fetch(`/api/sales/${id}`, {
+          method: "PUT",
+          body: JSON.stringify(form),
+          headers: {
+              "Content-type": "application/json; charset=UTF-8",
+              token: `${user.token}`,
+          }
+          })
+          router.push('/agent/sales')
+      } catch (error) {
+          console.log(error);
+          setErrMsg(error)
+      }
+    }   
+
+    return (
+        <Protected>
+            <div className="dashboard-container center w-100">
+                <h1>Edit Property Listing for {address}</h1>
+                <div className="agent-summary text-center">
+                    <InlineLink to="/agent/dashboard" text="Back to Dashboard" />
+                </div>
+            </div>
+            <div className='w-100'>
+                <Form 
+                    formArr={editFormArr}
+                    onSubmit={onSubmit}
+                    title='Edit Property Listing'
+                    submitBtn='Submit Changes'
+                    redirect={null}
+                />
+            {errMsg ? <Alert msg={errMsg} type='error'></Alert> : <></>}
+            </div>
+        </Protected>
+    )
+  }
+}
+
+export default EditListing
