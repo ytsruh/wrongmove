d1c5cd754a02cf7f9de71460fcfe073c6baad9bb
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2023-01-09 21:01:37 +0000

    commit before rebase

--- src/pages/sales/[id].jsx
+++ src/pages/sales/[id].jsx
@@ -1,5 +1,23 @@
+import { useRouter } from "next/router"
+import useFetchPublicData from "../../hooks/useFetchPublicData"
 
+export default function SalesListing() {
 
-export default function Egg() {
-    return <p>egg</p>
+    const router = useRouter()
+    const { id } = router.query
+    const { isLoading, serverError, apiData } = useFetchPublicData(`/api/public/sales/${id}`)
+    // const { address, agent, bathrooms, bedrooms, createdAt, description, images, keyFeatures, price, propertyType } = apiData.data
+
+    if(isLoading) return <h1>Loading...</h1>
+    if(serverError) return <h1>Server Error</h1>
+
+    return (
+        <div className="flex center w-100">
+            <div className="listing-image-grid">
+                <div>{apiData?.data?.address}</div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    )
 }
