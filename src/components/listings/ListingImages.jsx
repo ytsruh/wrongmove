ea3974f83e75560123d29a7dd3dcc34d4dc7dc9e
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2023-01-16 15:37:48 +0000

    added placeholder img to ListingImages

--- src/components/listings/ListingImages.jsx
+++ src/components/listings/ListingImages.jsx
@@ -3,14 +3,24 @@ import { useState, useEffect } from "react"
 export default function ListingImages (props) {
 
     const [apiData, setApiData] = useState(props.apiData)
+    console.log(apiData);
 
     return (
         <div className="four-column-grid listing-images gap-1">
-            {apiData.data.images.map((image, index) => (
+            {apiData.data.images[0] ? 
+            apiData.data.images.map((image, index) => (
                 <div onClick={() => {
                     props.onClick(image)
                 }} key={index} className="br-05 pointer" style={{backgroundImage: `url(${process.env.NEXT_PUBLIC_IMAGES_ENDPOINT + image.file})`}}></div>
-            ))}
+            ))
+            : 
+            <>
+                <div className="br-05 bg-img" style={{backgroundImage: 'url(https://via.placeholder.com/600x200.png?text=Placeholder+Image)'}}></div>
+                <div className="br-05 bg-img" style={{backgroundImage: 'url(https://via.placeholder.com/600x200.png?text=Placeholder+Image)'}}></div>
+                <div className="br-05 bg-img" style={{backgroundImage: 'url(https://via.placeholder.com/600x200.png?text=Placeholder+Image)'}}></div>
+                <div className="br-05 bg-img" style={{backgroundImage: 'url(https://via.placeholder.com/600x200.png?text=Placeholder+Image)'}}></div>
+            </>
+            }
         </div>
     )
 }
